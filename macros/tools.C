85443acc63fe4629e2a89e64a11500a5d9283f03
Simon Spannagel <simon.spannagel@desy.de>
2015-08-01 14:03:29 +0200

    Add new tool that returns the RMS of the center 96% of a distribution

--- macros/tools.C
+++ macros/tools.C
@@ -557,6 +557,47 @@ Double_t fitep0sigma( char* hs, int binlow=-999, int binhigh=999) {
 }
 
 
+// Returns the RMS including 96% of the histogram entries, cutting the tails:
+Double_t getRMS96(char* hs) {
+
+  bool debug = false;
+
+  TH1 *h = (TH1*)gDirectory->Get(hs);
+  if( h == NULL ){ cout << hs << " does not exist\n"; return 0; }
+
+  // Total entries:
+  double integral = h->GetEntries();
+  int maxbin = h->GetMaximumBin();
+  if(debug) cout << "entries=" << integral << " maxbin=" << maxbin << endl;
+
+  double subrange_integral = h->GetBinContent(maxbin);
+  int bin = 0;
+  while(subrange_integral < 0.96*integral) {
+    bin++;
+    // Add one bin to the left:
+    subrange_integral += h->GetBinContent(maxbin-bin);
+    // Add one bin to the right:
+    subrange_integral += h->GetBinContent(maxbin+bin);
+    if(debug) cout << "subrange " << (maxbin-bin) << "-" << (maxbin+bin) << ": entries=" << subrange_integral << endl;
+  }
+  if(debug) cout << "subrange " << (maxbin-bin) << "-" << (maxbin+bin) << " now has " << subrange_integral << " entries, this is " << (100.0*subrange_integral)/integral << "%" << endl;
+
+  // Correct by overshoot bin:
+  subrange_integral -= h->GetBinContent(maxbin+bin);
+  subrange_integral -= h->GetBinContent(maxbin-bin);
+  bin--;
+
+  int binlow = maxbin-bin;
+  int binhigh = maxbin+bin;
+  if(debug) cout << "subrange " << (maxbin-bin) << "-" << (maxbin+bin) << " now has " << subrange_integral << " entries, this is " << (100.0*subrange_integral)/integral << "%" << endl;
+
+  h->GetXaxis()->SetRange(binlow,binhigh); //to restrict range to bins binlow to binhigh
+  double rms96 = h->GetRMS(); //will return the RMS within the axis range
+
+  return rms96;
+}
+
+
 //------------------------------------------------------------------------------
 Double_t fitSkw( Double_t *x, Double_t *par )
 {
