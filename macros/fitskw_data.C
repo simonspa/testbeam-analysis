def509484bd3cc4fe5c1dc9897e58bafd7de021b
Simon Spannagel <simon.spannagel@desy.de>
2015-08-22 13:42:23 +0200

    Fix fitskw

--- macros/fitskw_data.C
+++ macros/fitskw_data.C
@@ -4,11 +4,12 @@ void fitskw_data() {
   cout << "inputdir!" << endl;
 }
 
-void fitskw_data(TString inputdir, int chip) {
+void fitskw_data(TString inputdir, int chip, int startrun, int stoprun) {
 
   // Get all runs for given chip:
   std::vector<int> runs = getruns(inputdir,chip);
   for(std::vector<int>::iterator run = runs.begin(); run != runs.end(); run++) {
+    if(*run < startrun || *run > stoprun) continue;
 
     TString fileName;
     fileName += inputdir;
@@ -23,7 +24,7 @@ void fitskw_data(TString inputdir, int chip) {
     Double_t tilt = gettilt(inputdir,*run,chip);
     std::pair<double,double> skwpar = fitskwlin("cmsdy0vsskw");
 
-    cout << tilt << " " << skwpar.first << " " << skwpar.second << end;
+    cout << tilt << " " << skwpar.first << " " << skwpar.second << endl;
     delete source;
   }
   
