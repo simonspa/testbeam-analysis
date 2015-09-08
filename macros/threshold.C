4e6bed738b5459db5d8248f53a5e481dfcd3d498
Simon Spannagel <simon.spannagel@desy.de>
2015-09-08 14:41:41 +0200

    Also plot theory threshold resolution values

--- macros/threshold.C
+++ macros/threshold.C
@@ -8,6 +8,7 @@
 #include "TGraph.h"
 #include "TSystem.h"
 #include "TStyle.h"
+#include "TString.h"
 #include "TROOT.h"
 
 #include <iomanip>
@@ -34,16 +35,17 @@ void threshold(const char* inputdir, int chip, int startrun, int stoprun) {
   setHHStyle(*gStyle);
 
   TCanvas *c1 = new TCanvas("c1","resolution",600,600);
-  TProfile *resolution = new TProfile("resolution"," ",56,28,84,0,60,"");
-  TProfile *resolution_corr = new TProfile("resolution_corr"," ",56,28,84,0,60,"");
+  TProfile *resolution = new TProfile("resolution"," ",56,1500,4000,3,770,"");
+  TProfile *resolution_corr = new TProfile("resolution_corr"," ",56,1500,4000,3,7,"");
 
   TCanvas *c2 = new TCanvas("c2","resolution",600,600);
-  TProfile *resolution_tel_subtracted = new TProfile("resolution_tel_subtracted"," ",56,28,84,0,60,"");
-  TProfile *resolution_corr_tel_subtracted = new TProfile("resolution_corr_tel_subtracted"," ",56,28,84,0,60,"");
+  TProfile *resolution_tel_subtracted = new TProfile("resolution_tel_subtracted"," ",56,1500,4000,3,7,"");
+  TProfile *resolution_corr_tel_subtracted = new TProfile("resolution_corr_tel_subtracted"," ",56,1500,4000,3,7,"");
 
   gStyle->SetOptStat(0);
 
   int nruns = 0, nfiducial = 0, nevents = 0;
+  Double_t tilt;
 
   // Get all runs for given chip:
   std::vector<int> runs = getruns(inputdir,chip,"-threshold");
@@ -75,14 +77,16 @@ void threshold(const char* inputdir, int chip, int startrun, int stoprun) {
     // Skew corrected:
     Double_t res_corr = fitep0sigma("cmsdyfctq4d");
     //Double_t res_corr = getRMS96("cmsdy0fctq4",92);
-    Double_t tilt = gettilt(inputdir,*run,chip,"-threshold");
+    tilt = gettilt(inputdir,*run,chip,"-threshold");
 
     // Collect statistics:
     nfiducial += ((TH1D*)gDirectory->Get("cmsdyfctq3"))->GetEntries();
     nevents += ((TH1D*)gDirectory->Get("trixylk"))->GetEntries();
 
     int threshold = gettrim(inputdir,*run,chip,"-threshold");
-    //if(*run == 20385 || *run == 20386) threshold = 123;
+
+    // From VCal to electrons: 50e/VCal DAC
+    Double_t ke = threshold*50;
 
     // Subtract telescope track resolution:
     Double_t dz = getdz(inputdir,*run,chip,"-threshold");
@@ -91,15 +95,15 @@ void threshold(const char* inputdir, int chip, int startrun, int stoprun) {
     Double_t res_tel_subtracted = TMath::Sqrt(res*res - tel_resolution*tel_resolution);
     Double_t res_corr_tel_subtracted = TMath::Sqrt(res_corr*res_corr - tel_resolution*tel_resolution);
 
-    cout << "run" << *run << " (chip" << chip << ") res " << res << " ressub " << res_tel_subtracted << " res_corr " << res_corr_tel_subtracted << " tilt " << tilt << " threshold " << threshold << endl;
+    cout << "run" << *run << " (chip" << chip << ") res " << res << " ressub " << res_tel_subtracted << " res_corr " << res_corr_tel_subtracted << " tilt " << tilt << " electrons " << ke << " threshold " << threshold << endl;
     //cout << *run << " " << tilt << " " << res_tel_subtracted << endl;
     //cout << " -> dz = " << dz << ", sigma_tel = " << tel_resolution << endl;
 
-    resolution->Fill(threshold,res,1);
-    resolution_corr->Fill(threshold,res_corr,1);
+    resolution->Fill(ke,res,1);
+    resolution_corr->Fill(ke,res_corr,1);
 
-    resolution_tel_subtracted->Fill(threshold,res_tel_subtracted,1);
-    resolution_corr_tel_subtracted->Fill(threshold,res_corr_tel_subtracted,1);
+    resolution_tel_subtracted->Fill(ke,res_tel_subtracted,1);
+    resolution_corr_tel_subtracted->Fill(ke,res_corr_tel_subtracted,1);
 
     nruns++;
     delete source;
@@ -115,7 +119,7 @@ void threshold(const char* inputdir, int chip, int startrun, int stoprun) {
   setLegendStyle(leg3);
 
   c1->cd();
-  resolution->SetTitle(";threshold [vcal];resolution x #left[#mum#right]");
+  resolution->SetTitle(";threshold [e];resolution x #left[#mum#right]");
   resolution->SetMarkerStyle(20);
   resolution->SetMarkerColor(1);
   resolution->Draw();
@@ -126,7 +130,7 @@ void threshold(const char* inputdir, int chip, int startrun, int stoprun) {
   DrawCMSLabels(nfiducial,5.2,0.045);
 
   c2->cd();
-  resolution_tel_subtracted->SetTitle(";threshold [vcal];resolution x #left[#mum#right]");
+  resolution_tel_subtracted->SetTitle(";threshold [e];resolution x #left[#mum#right]");
   resolution_tel_subtracted->SetMarkerStyle(20);
   resolution_tel_subtracted->SetMarkerColor(1);
   resolution_tel_subtracted->Draw();
@@ -136,32 +140,68 @@ void threshold(const char* inputdir, int chip, int startrun, int stoprun) {
   setStyleAndFillLegend(resolution_tel_subtracted,"data",leg2);
   DrawCMSLabels(nfiducial,5.2,0.045);
 
-  /*
-  int thickness = 294;
-  int threshold = 170; // maybe 160 or so?
-  //if(chip == 506) thickness = 308;
-  std::vector<double> vtilt = getsimulation("tilt", chip,thickness,threshold);
-  std::vector<double> veta = getsimulation("eta", chip,thickness,threshold);
-  std::vector<double> vres = getsimulation("res", chip,thickness,threshold);
-  std::vector<double> vresskw = getsimulation("resskw", chip,thickness,threshold);
 
-  if(!vtilt.empty()) {
+  int thickness = 294;
+
+  std::vector<double> vthr;
+  std::vector<double> vres;
+
+  // Read thresholds
+  for(Int_t thr = 150; thr < 420; thr += 10) {
+
+    TString myfile;
+    myfile.Form("simulation/sim%i_%iskw_thr%i.dat",thickness,chip,thr);
+  
+    ifstream SIMstream( myfile );
+    if( !SIMstream ) { continue;}
+
+    // Read file by lines:
+    string rl;
+    double tlt;
+    int run;
+    int nev;
+    double ry;
+    double ry_skwcorr;
+    double ncol;
+    double lanpk;
+    double turn;
+    double edge;
+
+    while( SIMstream.good() && ! SIMstream.eof() ) {
+
+      getline( SIMstream, rl ); // read one line  = event into string
+      istringstream simrun( rl ); // tokenize string
+
+      simrun >> run;
+      simrun >> tlt; // [deg]
+      simrun >> turn;
+      simrun >> nev;
+      simrun >> ry;
+      simrun >> ry_skwcorr;
+      simrun >> ncol;
+      simrun >> lanpk;
+      simrun >> edge;
+
+      if(tilt < (tlt+0.5)) {
+	vthr.push_back(10*thr);
+	vres.push_back(sqrt( ry*ry - restel_sim*restel_sim )); // subtract telescope
+	cout << "sim tilt " << tlt << " res " << ry << " ressub " << sqrt( ry*ry - restel_sim*restel_sim ) << " electrons " << (thr*10) << endl;
+	break;
+      }
+    } // while lines
+  }
+
+  if(!vthr.empty()) {
     c2->cd();
-    TGraph *si = new TGraph( vtilt.size(), &(vtilt[0]), &(vres[0]) ); // sim
+    TGraph *si = new TGraph( vthr.size(), &(vthr[0]), &(vres[0]) ); // sim
     si->SetLineColor(2);
     si->SetLineWidth(3);
     si->SetMarkerColor(2);
-    resolution_tel_subtracted->GetXaxis()->SetRangeUser(vtilt.front(), vtilt.back());
+    resolution_tel_subtracted->GetXaxis()->SetRangeUser(vthr.front(), vthr.back());
     si->Draw("PL"); // without axis option: overlay
     setStyleAndFillLegend(si,"sim",leg2);
-
-    TGraph *siskw = new TGraph( vtilt.size(), &(vtilt[0]), &(vresskw[0]) ); // sim
-    siskw->SetLineColor(kRed-7);
-    siskw->SetLineWidth(3);
-    siskw->SetMarkerSize(0);
-    if(draw_skwcorr) siskw->Draw("PL"); // without axis option: overlay
   }
-  */
+
   c2->cd();
   leg2->Draw();
 
