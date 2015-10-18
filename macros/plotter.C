9982960eceeff1a875475a1639f119d2fa2fb8aa
Simon Spannagel <simon.spannagel@desy.de>
2015-10-18 13:15:19 +0200

    Add plot option for free CMS label

--- macros/plotter.C
+++ macros/plotter.C
@@ -251,6 +251,25 @@ void DrawCMSLabels(double lumi, double energy, double textSize) {
   label->Draw("same");
 }
 
+void DrawFreeCMSLabels(char* text, double textSize) {
+
+  //const char *text = "%2.1f #times 10^{6} clusters (fiducial) (%1.1f GeV)";
+  //char *text = "%2.1f #times 10^{6} clusters (fiducial)";
+    
+  TPaveText *label = new TPaveText();
+  label->SetX1NDC(gStyle->GetPadLeftMargin());
+  label->SetY1NDC(1.0-gStyle->GetPadTopMargin());
+  label->SetX2NDC(1.0-gStyle->GetPadRightMargin());
+  label->SetY2NDC(1.0);
+  label->SetTextFont(42);
+  label->AddText(text);
+  label->SetFillStyle(0);
+  label->SetBorderSize(0);
+  if (textSize!=0) label->SetTextSize(textSize);
+  label->SetTextAlign(32);
+  label->Draw("same");
+}
+
 void setLegendStyle(TLegend *leg) {
 
   Double_t x1 = 0.5216107;
