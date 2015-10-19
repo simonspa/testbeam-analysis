00255406b991f12fcb23414b7ddd033653b72676
Simon Spannagel <simon.spannagel@desy.de>
2015-10-19 15:44:50 +0200

    Improve plotskw script

--- macros/plotskw.C
+++ macros/plotskw.C
@@ -1,17 +1,49 @@
-void plotskw() {
+#include "TH1.h"
+#include "TMath.h"
+#include "TF1.h"
+#include "TLegend.h"
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TProfile.h"
+#include "TGraph.h"
+#include "TSystem.h"
+#include "TStyle.h"
+#include "TROOT.h"
 
-  TCanvas *c1 = new TCanvas("c1","par0",600,600);
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "constants.h"
+#include "tools.C"
+#include "plotter.C"
+
+using namespace std;
+
+bool cmslogo = false;
+
+void plotskw() {
+  std::cout << "Run plotskw(file)" << std::endl;
+}
+
+void plotskw(TString myfile, int chip) {
+
+  // Set the histogram styles:
+  setHHStyle(*gStyle);
+  gStyle->SetTitleYOffset(1.1);
+
+  TCanvas *c1 = new TCanvas("c1","par0",700,700);
   TProfile *ppar0 = new TProfile("par0"," ",90,0,90,-10,10,"");
 
-  TCanvas *c2 = new TCanvas("c2","par1",600,600);
+  TCanvas *c2 = new TCanvas("c2","par1",700,700);
   TProfile *ppar1 = new TProfile("par1"," ",90,0,90,-2000,1000,"");
-
 
   Double_t tilt, par0, par1;
 
   ifstream in;
   string line;
-  in.open("simulation/skwcorr_504_294um_gap30.dat");
+  in.open(myfile);
 
   // Skip first line:
   getline(in,line);
@@ -31,6 +63,7 @@ void plotskw() {
       if(i == 2) par1 = atof(str.c_str());
       i++;
     }
+    cout << "tilt " << tilt << " p0 " << par0 << " p1 " << par1 << endl;
     ppar0->Fill(tilt,par0,1);
     ppar1->Fill(tilt,par1,1);
   }
@@ -40,7 +73,13 @@ void plotskw() {
   ppar0->Draw();
 
   c2->cd();
+  setStyle(ppar1,"sim");
+  ppar1->SetTitle(";tilt angle [#circ];skew slope");
+  ppar1->GetYaxis()->SetTitleOffset(1.2);
+  ppar1->GetXaxis()->SetRangeUser(0,tilt);
   ppar1->Draw();
+  if(chip == 506) DrawFreeCMSLabels("Simulation, 150 #mum pitch",0);
+  else DrawFreeCMSLabels("Simulation, 100 #mum pitch",0);
 
   in.close();
 }
