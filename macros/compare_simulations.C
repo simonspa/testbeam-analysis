affbac80790e2b50be36524b5307e743559c9a1d
Simon Spannagel <simon.spannagel@desy.de>
2015-10-22 22:48:52 +0200

    Pimp simulation comparison macro

--- macros/compare_simulations.C
+++ macros/compare_simulations.C
@@ -26,15 +26,24 @@ void compare_simulations() {
 
 void compare_simulations(int chip) {
 
-  TCanvas *c2 = new TCanvas("c2","resolution",600,600);
+  TCanvas *c2 = new TCanvas("c2","resolution",700,700);
 
   gStyle->SetOptStat(0);
+  // Set the histogram styles:
+  setHHStyle(*gStyle);
+  gStyle->SetTitleYOffset(1.1);
 
-  std::vector<double> vtilt_gap30 = getsimulation("tilt", chip, 294, 170);
-  std::vector<double> vres_gap30 = getsimulation("res", chip, 294, 170);
+  int thickness = 294;
+  if(chip == 506) thickness = 308;
 
-  std::vector<double> vtilt_dot1 = getsimulation("tilt", chip, 294, 150);
-  std::vector<double> vres_dot1 = getsimulation("res", chip, 294, 150);
+  int threshold = 170;
+  if(chip == 506) threshold = 200;
+
+  std::vector<double> vtilt_gap30 = getsimulation("tilt", chip, thickness, threshold);
+  std::vector<double> vres_gap30 = getsimulation("resskw", chip, thickness, threshold);
+
+  std::vector<double> vtilt_dot1 = getsimulation("tilt", chip, thickness, threshold,true);
+  std::vector<double> vres_dot1 = getsimulation("resskw", chip, thickness, threshold,true);
 
   TLegend *leg = new TLegend();
   setLegendStyle(leg);
@@ -42,18 +51,23 @@ void compare_simulations(int chip) {
   if(!vtilt_gap30.empty()) {
     c2->cd();
     TGraph *si = new TGraph( vtilt_gap30.size(), &(vtilt_gap30[0]), &(vres_gap30[0]) ); // sim
+    setStyle(si,"sim");
     si->SetLineColor(2);
     si->SetLineWidth(3);
     si->SetMarkerColor(2);
+    if(chip == 506) si->SetTitle(";tilt angle #omega [#circ];resolution y #left[#mum#right]");
+    else si->SetTitle(";tilt angle #alpha [#circ];resolution x #left[#mum#right]");
     si->Draw("APL");
-    setStyleAndFillLegend(si,"gap30",leg);
+    setStyle(si,"dot1");
+    leg->AddEntry(si, "Linear fit",  "l");
 
     TGraph *si_dot1 = new TGraph( vtilt_dot1.size(), &(vtilt_dot1[0]), &(vres_dot1[0]) ); // sim
     si_dot1->SetLineColor(2);
     si_dot1->SetLineWidth(3);
     si_dot1->SetMarkerColor(2);
     si_dot1->Draw("PL"); // without axis option: overlay
-    setStyleAndFillLegend(si_dot1,"dot1",leg);
+    setStyle(si_dot1,"gap30");
+    leg->AddEntry(si_dot1, "3rd order polynomial fit",  "l");
 
     leg->Draw();
   }
